80af473484ba13d90da2e00556b8014059959093
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-19 00:45:50 +0100

    GamerProject and Interface created

---
+++ KampIntro/GameProject/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    //MicroService
+    class GameManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GameManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayit oldu");
+            }
+            else
+            {
+                Console.WriteLine("Kayit basarisiz oldu");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayit silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayit guncellendi");
+        }
+    }
+}
---
+++ KampIntro/GameProject/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1985 && gamer.IdentityNumber == 12345
+                && gamer.FirstName == "MUHAMMED" && gamer.LastName == "AZMAN")
+            {
+                Console.WriteLine("Validation Manager Calisti");
+                return true;
+            } 
+            else
+            {
+                Console.WriteLine("Validation Manager Hata Alindi");
+                return false;
+            }
+        }
+    }
+}
