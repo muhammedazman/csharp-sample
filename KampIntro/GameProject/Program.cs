0a655187c6fba0fcbf66ed462d32865a34726846
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-19 00:56:18 +0100

    systax update

--- KampIntro/GameProject/Program.cs
+++ KampIntro/GameProject/Program.cs
@@ -6,6 +6,14 @@ namespace GameProject
     {
         static void Main(string[] args)
         {
+            GameManager gameManager = new GameManager(new UserValidationManager());
+            gameManager.Add(new Gamer { 
+                Id = 1, 
+                IdentityNumber=12345, 
+                FirstName="MUHAMMED", 
+                LastName="AZMAN", 
+                BirthYear=1985}
+            );
             Console.WriteLine("Hello World!");
         }
     }
